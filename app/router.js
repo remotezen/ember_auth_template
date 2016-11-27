fe8d53f698743b15e6fb253c1e9361e6e5fa254f
remotezen <remotezen@bell.net>
2016-11-26 21:50:11 -0500

    added libraries routes

--- app/router.js
+++ app/router.js
@@ -12,10 +12,14 @@ Router.map(function() {
   this.route('secret');
   this.route('about');
   this.route('contact');
+  this.route('libraries', function() {
+    this.route('new');
+  });
 
   this.route('admin', function() {
     this.route('invitations');
   });
+  
 });
 
 export default Router;
